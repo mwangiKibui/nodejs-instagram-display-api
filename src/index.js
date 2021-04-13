0fa33b89f6e63121ff1a70e725a7409a846d96b4
mwangikibui <kenmwangi250@gmail.com>
2021-04-13 11:18:37 +0300

    fix: removed multiline for login url

--- src/index.js
+++ src/index.js
@@ -22,11 +22,7 @@ app.get('/login', (req,res) => {
 
     // we are loading another external link
 
-    return res.redirect(`https://api.instagram.com/oauth/authorize
-    ?client_id=936479663836449
-    &redirect_uri=https://insta-display-api.herokuapp.com/auth/
-    &scope=user_profile,user_media
-    &response_type=code`);
+    return res.redirect(`https://api.instagram.com/oauth/authorize?client_id=936479663836449&redirect_uri=https://insta-display-api.herokuapp.com/auth/&scope=user_profile,user_media&response_type=code`);
 
 })
 
