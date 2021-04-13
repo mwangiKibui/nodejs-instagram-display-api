4b9556c743b51009d1ae6091c58010e710c6c5ca
mwangikibui <kenmwangi250@gmail.com>
2021-04-13 09:58:53 +0300

    fix: added authentication

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const axios = require('axios');
 const app = express();
 const PORT = process.env.PORT || 4000;
 
@@ -9,9 +10,35 @@ app.use(express.json());
 app.use(express.urlencoded({ extended:false }));
 
 //the simple routes
-app.use('/', (req,res) => {
+app.get('/', (req,res) => {
     return res.send('Hello there. The server is up.')
 });
+
+app.use('/auth', async (req,res) => {
+
+    // handle request to insta API.
+    let response
+    try {
+
+        response = await axios.default.get(`https://api.instagram.com/oauth/authorize`,{
+            params:{
+                client_id:"936479663836449",
+                redirect_uri:"https://insta-display-api.herokuapp.com/auth",
+                scope:"user_profile,user_media",
+                response_code:"code"
+            }
+        })
+
+    }catch(error){
+
+        console.error(`error`,error);
+
+        return res.send(error);
+    };
+
+    console.log('response', response.data);
+    return;
+})
 
 // starting the server
 app.listen(PORT, () => console.log(`app started on port:${PORT}`));
